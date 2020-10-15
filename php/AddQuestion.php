707f7daf8dbfe46fd53fdfe648ee1014a0475928
asierelursw <asierelursw@gmail.com>
2020-10-15 23:39:52 +0200

    Update AddQuestion.php

--- php/AddQuestion.php
+++ php/AddQuestion.php
@@ -7,9 +7,17 @@
   <?php include '../php/Menus.php' ?>
   <section class="main" id="s1">
     <div>
-
-			Código PHP para añadir una pregunta sin imagen
-
+	<?php
+	$link = mysqli_connect("localhost", "root", "", "quiz");
+	$sql="INSERT INTO preguntas VALUES'('Correo', 'Pregunta', 'Correcta', 'Incorrecta1', 'Incorrecta2', 'Incorrecta3', 'Dificultad', 'Tema')'
+	('$_POST[mail]','$_POST[pregunta]','$_POST[correcta]','$_POST[falsa1]','$_POST[falsa2]','$_POST[falsa3]','$_POST[dificultad]','$_POST[tema]');";
+	if (!mysqli_query($link ,$sql))
+	{
+		die('Error: ' . mysqli_error($link));
+	}
+	echo "1 record added";
+	mysqli_close($link);
+	?>
     </div>
   </section>
   <?php include '../html/Footer.html' ?>
