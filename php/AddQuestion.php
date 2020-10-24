d676ec7f802cfafbb3f2e8297940b6ac7e06adc8
asierelursw <asierelursw@gmail.com>
2020-10-24 16:48:05 +0200

    Update AddQuestion.php

--- php/AddQuestion.php
+++ php/AddQuestion.php
@@ -2,20 +2,22 @@
 <html>
 <head>
   <?php include '../html/Head.html'?>
+  <?php include '../php/DbConfig.php'?>
 </head>
 <body>
-  <?php include '../php/Menus.php' ?>
+  <?php include '../php/Menus.php';?>
+  
   <section class="main" id="s1">
     <div>
 	<?php
-	$link = mysqli_connect("localhost", "root", "", "quiz");
-	$sql="INSERT INTO preguntas VALUES'('Correo', 'Pregunta', 'Correcta', 'Incorrecta1', 'Incorrecta2', 'Incorrecta3', 'Dificultad', 'Tema')'
-	('$_POST[mail]','$_POST[pregunta]','$_POST[correcta]','$_POST[falsa1]','$_POST[falsa2]','$_POST[falsa3]','$_POST[dificultad]','$_POST[tema]');";
+	$link = mysqli_connect($server, $user, $pass, $basededatos);
+	$sql="INSERT INTO preguntas(Correo, Pregunta, Correcta, Incorrecta1, Incorrecta2, Incorrecta3, Dificultad, Tema) VALUES('$_REQUEST[user_mail]','$_REQUEST[pregunta]','$_REQUEST[correcta]','$_REQUEST[falsa1]','$_REQUEST[falsa2]','$_REQUEST[falsa3]','$_REQUEST[dificultad]','$_REQUEST[tema]');";
 	if (!mysqli_query($link ,$sql))
 	{
 		die('Error: ' . mysqli_error($link));
 	}
 	echo "1 record added";
+	//echo "<p> <a href='verdatos.php'> Ver registros </a>";
 	mysqli_close($link);
 	?>
     </div>
