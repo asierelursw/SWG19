c50262e6645067b79435126bbb9a7867eb29d92b
asierelursw <asierelursw@gmail.com>
2020-10-24 16:48:13 +0200

    Update ShowQuestions.php

--- php/ShowQuestions.php
+++ php/ShowQuestions.php
@@ -2,13 +2,42 @@
 <html>
 <head>
   <?php include '../html/Head.html'?>
+  <?php include '../php/DbConfig.php'?>
+  <style>
+      table, th, td { 
+                border: 2px black; 
+                text-align:center; 
+            } 
+            th, td { 
+                padding: 15px; 
+                background-color: white; 
+            } 
+  </style> 
 </head>
 <body>
   <?php include '../php/Menus.php' ?>
   <section class="main" id="s1">
     <div>
-      Código PHP para mostrar una tabla con las preguntas de la BD.<br>
-      La tabla no incluye las imágenes
+		<?php
+			$link = mysqli_connect($server, $user, $pass, $basededatos);
+			if (!mysqli_query($link ,$sql))
+			{
+				die('Error: ' . mysqli_error($link));
+			} else {
+                $sql = "SELECT * FROM preguntas;";
+                $resultado = mysqli_query ($link,$sql);
+				
+				echo "<table border=1>";
+                echo "    <tr><th><b>Autor</b></th><th><b>Enunciado</b></th><th><b>Respuesta</b></th></tr>";
+                
+                while( $row = mysqli_fetch_array( $resultado)){
+                    echo "    <tr><td>".$row['mail']."</td><td>".$row['enunciado']."</td><td>".$row['respuesta_correcta']."</td></tr>";
+                }
+                
+                echo "</table>";
+                
+                mysqli_close($link);
+            }
     </div>
   </section>
   <?php include '../html/Footer.html' ?>
