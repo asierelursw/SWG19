5f35e61ce7308689da42da69c0f3b3596ceebbb2
asierelursw <asierelursw@gmail.com>
2020-11-17 23:03:11 +0100

    Update HandlingQuizesAjax.php

--- php/HandlingQuizesAjax.php
+++ php/HandlingQuizesAjax.php
@@ -1,12 +1,30 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-  <?php include '../html/Head.html'?>
-  <script src = '../js/ValidateFieldsQuestion.js'> </script>
-  <script src= '../js/jquery-3.4.1.min.js'> </script>
-  <script src= '../js/AddQuestionAjax.js'> </script>
-  <script src= '../js/ShowQuestionAjax.js'> </script>
+    <?php include '../html/Head.html'?>
+    <script src='../js/ValidateFieldsQuestion.js'>
+    </script>
+    <script src='../js/jquery-3.4.1.min.js'>
+    </script>
+    <script src='../js/AddQuestionAjax.js'>
+    </script>
+    <script src='../js/ShowQuestionsAjax.js'>
+    </script>
+    <style>
+        #div1 {
+            margin: auto;
+            overflow: scroll;
+            height: 100%;
+            width: 100%;
+        }
+        
+        * {
+            margin: auto;
+        }
+    </style>
 </head>
+
 <body>
     <?php
     if(isset($_GET['email'])){
@@ -16,96 +34,101 @@
         include "../php/Menus.php";
     }
   ?>
-  <section class="main" id="s1">
-    <div>
-	<form id='fquestion' name='fquestion' action= "" method="Post">
- <ul>
- <center>
-  <li>
-    <label for="mail">Correo*:</label>
-    <input type="email" id="mail" name="user_mail" value="<?php echo $email; ?>" size= "52" readonly>
-  </li>
-  <p>
-  <br/>
-  </p>
-  <li>
-    <label for="pregunta">Pregunta*:</label>
-    <input type="text" id="pregunta" name="pregunta" size= "50">
-  </li>
-  <p>
-  <br/>
-  </p>
-  <li>
-    <label for="respuesta">Respuesta correcta*:
-	<br/>
-	</label>
-    <input type="textarea" id="correcta" name="correcta" size="62"> </textarea>
-  </li>
-  <p>
-  <br/>
-  </p>
-  <li>
-    <label for="respuesta">Respuesta incorrecta*:
-	<br/>
-	</label>
-    <input type="textarea" id="falsa1" name="falsa1" size="62"> </textarea>
-  </li>
-  <p>
-  <br/>
-  </p>
-  <li>
-    <label for="respuesta">Respuesta incorrecta*:
-	<br/>
-	</label>
-    <input type="textarea" id="falsa2" name="falsa2" size="62"> </textarea>
-  </li>
-  <p>
-  <br/>
-  </p>
- <li>
-    <label for="respuesta">Respuesta incorrecta*:
-	<br/>
-	</label>
-    <input type="textarea" id="falsa3" name="falsa3" size="62"></textarea>
-  </li>
-  <p>
-  <br/>
-  </p>
-   <li>
-    <label for="dificultad"> Dificultad de la pregunta:</label>
-	<select name="dificultad">
+        <section class="main" id="s1">
+            <div id="div1">
+                <form id='fquestion' name='fquestion' method="POST">
+                    <ul>
+                        <center>
+                            <li>
+                                <label for="mail">Correo*:</label>
+                                <input type="email" id="mail" name="user_mail" value="<?php echo $email; ?>" size="52" readonly>
+                            </li>
+                            <p>
+                                <br />
+                            </p>
+                            <li>
+                                <label for="pregunta">Pregunta*:</label>
+                                <input type="text" id="pregunta" name="pregunta" size="50">
+                            </li>
+                            <p>
+                                <br />
+                            </p>
+                            <li>
+                                <label for="respuesta">Respuesta correcta*:
+                <br/>
+              </label>
+                                <input type="textarea" id="correcta" name="correcta" size="62"> </textarea>
+                            </li>
+                            <p>
+                                <br />
+                            </p>
+                            <li>
+                                <label for="respuesta">Respuesta incorrecta*:
+                <br />
+              </label>
+                                <input type="textarea" id="falsa1" name="falsa1" size="62"> </textarea>
+                            </li>
+                            <p>
+                                <br />
+                            </p>
+                            <li>
+                                <label for="respuesta">Respuesta incorrecta*:
+                <br />
+              </label>
+                                <input type="textarea" id="falsa2" name="falsa2" size="62"> </textarea>
+                            </li>
+                            <p>
+                                <br />
+                            </p>
+                            <li>
+                                <label for="respuesta">Respuesta incorrecta*:
+                <br />
+              </label>
+                                <input type="textarea" id="falsa3" name="falsa3" size="62"></textarea>
+                            </li>
+                            <p>
+                                <br />
+                            </p>
+                            <li>
+                                <label for="dificultad"> Dificultad de la pregunta:</label>
+                                <select name="dificultad">
 
-	<option>Facil</option>
+                <option>Facil</option>
 
-	<option>Medio</option>
+                <option>Medio</option>
 
-	<option>Dificil</option>
-	</li>
-	<p>
-	<br/>
-	</p>
-	<p>
-	<input type="reset" value="BORRAR" onclick="Borrar()">
-	</p>
-	<li>
-    <label for="respuesta">Tema*:
-	<br/>
-	</label>
-    <input type="textarea" id="tema" name="tema" size="62"></textarea>
-  </li>
-</select>
- </ul> 
- 
- <p>
- <br/>
- </p>
- <input type="button" value="Enviar" id="Enviar"  style="height:100px; width:150px" onclick ="AñadirPregunta()">
- </center>
-</form>
- <input type="button" value="Mostrar Preguntas" id="Mostrar"style="height:100px; width:150px" onclick ="VerPreguntas($email)">
-	<div id="result"></div>
-  
-  </section>
-  <?php include '../html/Footer.html' ?>
+                <option>Dificil</option>
+            </li>
+            <p>
+              <br />
+            </p>
+            <p>
+              <input type="reset" value="BORRAR" onclick="Borrar()">
+            </p>
+            <li>
+              <label for="respuesta">Tema*:
+                <br />
+              </label>
+              <input type="textarea" id="tema" name="tema" size="62"></textarea>
+            </li>
+            </select>
+                    </ul>
+
+                    <p>
+                        <br />
+                    </p>
+                    <input type="button" value="Enviar" id="Enviar" style="height:20px; width:300px" onclick="AñadirPregunta()">
+                    <p>
+                        <br />
+                    </p>
+
+                </form>
+                <input type="button" value="Mostrar Preguntas" id="Mostrar" style="height:20px; width:300px" onclick="VerPreguntas()">
+                <div id="result"></div>
+                </center>
+        </section>
+
+        <?php include '../html/Footer.html' ?>
 </body>
+
 </html>
