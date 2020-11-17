0ea21046c6736855e09fe3bdae255466e70c6cac
asierelursw <asierelursw@gmail.com>
2020-11-17 23:02:59 +0100

    Update ShowQuestionsAjax.js

--- js/ShowQuestionsAjax.js
+++ js/ShowQuestionsAjax.js
@@ -1,5 +1,5 @@
 
-function VerPreguntas(email){
+/*function VerPreguntas(email){
     $.ajax({
         url: 'ShowXMLQuestions.php?email='+ email,
         beforeSend:function(){$("#result").html('<div><img src="loading.gif"/></div>');},
@@ -12,5 +12,15 @@ function VerPreguntas(email){
         $("#result").fadeIn().html('<p class="error"><strong>El servidor parece que no responde</p>');
         }
         });
-}
+}*/
 //este con jQuery
+
+function VerPreguntas() {
+  if(XMLHttpRequest) xhr = new XMLHttpRequest();
+  xhr.open("GET", "../php/ShowXMLQuestions.php", true);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4 && xhr.status == 200)
+      document.getElementById("result").innerHTML = xhr.responseText;
+  };
+  xhr.send("");
+}
