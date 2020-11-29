c4675681d1bc8f92148144fb1162405f26590757
asierelursw <asierelursw@gmail.com>
2020-11-29 16:33:34 +0100

    Create HandlingAccounts.php

---
+++ php/HandlingAccounts.php
@@ -0,0 +1,46 @@
+<?php 
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <?php include '../html/Head.html'?>
+  <?php include '../php/DbConfig.php'?>
+</head>
+<body>
+<?php
+    if(isset($_SESSION['usuario'])){
+        include "../php/MenusRegistrados.php";
+    }
+?>
+  
+  <section class="main" id="s1">
+    <div>
+
+		<?php 
+        if($_SESSION['usuario']=='admin@ehu.es'){
+        $link = mysqli_connect($server, $user, $pass, $basededatos);
+
+            $usuarios = mysqli_query( $link,"SELECT * FROM Usuario");
+    			   if (!$usuarios){
+    				  die('Error: ' . mysqli_error());
+    			   }
+
+            echo "<table border=1>";
+            echo "<tr><th>Correo</th><th>Contraseña</th><th>Nombre y Apellido</th><th>Tipo de Usuario</th><th>Bloquear/Desbloquear</th><th>BORRAR</th></tr>"; 
+
+        while($row = mysqli_fetch_array($usuarios)){ 
+            echo "<tr><td>" . $row['Correo'] . "</td><td>" . $row['Pass'] . "</td><td>" . $row['NomApe'] . "</td><td>" .$row['TipoUsuario']."</td><td>" .$row['Bloqueado/Desbloqueado']. /*"</td><td>" <a href='RemoveUser.php?email='.$row['Correo'].><button></button></a>*/ "</td></tr>";
+            //<button id="Bloquear/Desbloquear" name="Bloquear/Desbloquear" onClick='location.href="ChangeUserState.php"'></button>
+        }
+
+echo "</table>";
+      } else {
+        echo"NO PUEDES ACCEDER A ESTA PÁGINA SI NO ERES ADMINISTRADOR";
+      }
+    ?>
+    </div>
+  </section>
+  <?php include '../html/Footer.html' ?>
+</body>
+</html>
