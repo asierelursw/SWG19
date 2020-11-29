4c9ca6da41ee25ceed8db6a70a19c536cb5b64f8
asierelursw <asierelursw@gmail.com>
2020-11-29 16:33:40 +0100

    Update Layout.php

--- php/Layout.php
+++ php/Layout.php
@@ -1,16 +1,19 @@
+<?php 
+session_start();
+?>
 <!DOCTYPE html>
 <html>
 <head>
   <?php include '../html/Head.html'?>
 </head>
 <body>
-  <?php
-    if(isset($_GET['email'])){
+<?php
+    if(isset($_SESSION['usuario'])){
         include "../php/MenusRegistrados.php";
     } else {
         include "../php/Menus.php";
     }
-  ?>
+?>
   <section class="main" id="s1">
     <div>
 
