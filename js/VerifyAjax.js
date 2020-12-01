1cd4156b32cd906c564b15243e0f463da1833656
asierelursw <asierelursw@gmail.com>
2020-12-01 13:53:39 +0100

    Update VerifyAjax.js

--- js/VerifyAjax.js
+++ js/VerifyAjax.js
@@ -1,23 +1,27 @@
 function VerificarVip() {
     var email = document.getElementById('user_mail').value;
-
+    var bool = false;
     if (XMLHttpRequest) xhr = new XMLHttpRequest();
     xhr.open("GET", "../php/ClientVerifyEnrollment.php?email=" + email, true);
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4 && xhr.status == 200)
             document.getElementById("vip").innerHTML = xhr.responseText;
         if (xhr.responseText == "<p style='color:green;'> Usuario Vip </p>") {
-            document.getElementById('Enviar').disabled = false;
+            bool = true;
+            //document.getElementById('Enviar').disabled = false;
         } else {
-            document.getElementById('Enviar').disabled = true;
+            bool = false;
+            //document.getElementById('Enviar').disabled = true;
         }
     };
     xhr.send("");
+    return bool;
 }
 
 function VarificarPass() {
 
     var pass = document.getElementById('pass1').value;
+    var bool = false;
 
     if (XMLHttpRequest) xhr = new XMLHttpRequest();
     xhr.open("GET", "../php/ClientVerifyPass.php?pass1=" + pass, true);
@@ -27,11 +31,25 @@ function VarificarPass() {
             document.getElementById("validpass").innerHTML = xhr.responseText;
 
         if (xhr.responseText == "<p style='color:green;'> Contraseña Valida </p>") {
-            document.getElementById('Enviar').disabled = false;
+            bool = true;
+            //document.getElementById('Enviar').disabled = false;
         } else {
-            document.getElementById('Enviar').disabled = true;
+            bool = false;
+            //document.getElementById('Enviar').disabled = true;
+
         }
 
     };
     xhr.send("");
+    return bool;
+}
+
+function activar() {
+    var pass = document.getElementById('validpass').value;
+    var email = document.getElementById('vip').value;
+    if ((VerificarVip() == false && VarificarPass() == false) || (VerificarVip() == false) || (VarificarPass() == false)) {
+        document.getElementById('Enviar').disabled = true;
+    } else {
+        document.getElementById('Enviar').disabled = false;
+    }
 }
