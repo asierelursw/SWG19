2fa54fc23149c58755bfe6da8d55fc35dc3b7113
asierelursw <asierelursw@gmail.com>
2020-12-20 22:37:30 +0100

    Create CountQuestionsAjax.js

---
+++ js/CountQuestionsAjax.js
@@ -0,0 +1,12 @@
+setInterval(function () {
+	if (XMLHttpRequest)
+		xhr = new XMLHttpRequest();
+	xhr.open('GET', '../php/CountXMLQuestions.php?email=' + document.getElementById('user_mail').value, true);
+	xhr.onreadystatechange = function () {
+
+		if (xhr.readyState == 4 && xhr.status == 200) {
+			document.getElementById('contPreguntas').innerHTML = xhr.responseText;
+		}
+	}
+	xhr.send('');
+}, 10000);
