11e9f95cb425f967dd3d8d60b9e9df091debb8ea
asierelursw <asierelursw@gmail.com>
2020-12-20 22:37:40 +0100

    Update ClientVerifyPass.php

--- php/ClientVerifyPass.php
+++ php/ClientVerifyPass.php
@@ -10,14 +10,14 @@
     //creamos el objeto de tipo soapclient.
     //http://www.mydomain.com/server.php se refiere a la url
     //donde se encuentra el servicio SOAP que vamos a utilizar.
-
+    $ticket='1010';
     $soapclient = new nusoap_client('https://crescive-discontinu.000webhostapp.com/LabBDAsierCruzElurSalgueira/php/VerifyPassWS.php?wsdl',true);
 
     //Llamamos la función que habíamos implementado en el Web Service
     //e imprimimos lo que nos devuelve
     if(isset($cont)){
 
-        $result = $soapclient->call('Validar',array('x'=>$cont));
+        $result = $soapclient->call('Validar',array('x'=>$cont,'y'=>$ticket));
 
         if($result =="INVALIDA"){
         echo  "<p style='color:red;'> Contraseña No Valida </p>";
