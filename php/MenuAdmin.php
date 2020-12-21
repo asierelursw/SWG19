dd608ef8c0023a0987c1564b71a5514039237781
asierelursw <asierelursw@gmail.com>
2020-12-21 21:29:54 +0100

    Update MenuAdmin.php

--- php/MenuAdmin.php
+++ php/MenuAdmin.php
@@ -6,7 +6,6 @@ echo"
 </header>
 <nav class='main' id='n1' role='navigation'>
   <span><a href='Layout.php'>Inicio</a></span>
-  <span><a href='HandlingQuizesAjax.php'> Insertar/Ver Preguntas</a></span>
   <span><a href='HandlingAccounts.php'> Administrar cuentas</a></span>
   <span><a href='Credits.php'>Creditos</a></span>
 </nav>";
