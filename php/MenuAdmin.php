5989b71b0fc4b4588bb047a91cbc72e0c0977e18
asierelursw <asierelursw@gmail.com>
2020-12-22 12:40:45 +0100

    Update MenuAdmin.php

--- php/MenuAdmin.php
+++ php/MenuAdmin.php
@@ -1,7 +1,14 @@
 <?php
+include '../php/DbConfig.php';
+$link = mysqli_connect($server, $user, $pass, $basededatos);
+$sql="SELECT Imagen FROM Usuario WHERE Usuario.Correo =".$_SESSION['usuario'].";";
+$_SESSION['img'] = $sql;
+
 echo"
 <div id='page-wrap'>
 <header class='main' id='h1'>
+<span class='right'><a>".$_SESSION['usuario']."</a>
+<img src='".base64_decode($sql)."'>
         <span class='right'><a href='LogOut.php'>Logout</a></span>
 </header>
 <nav class='main' id='n1' role='navigation'>
