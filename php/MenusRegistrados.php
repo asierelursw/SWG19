9b5c79380c6c6bf8aa248a1b7eec902585c002eb
asierelursw <asierelursw@gmail.com>
2020-12-22 12:30:39 +0100

    Update MenusRegistrados.php

--- php/MenusRegistrados.php
+++ php/MenusRegistrados.php
@@ -1,14 +1,15 @@
 <?php
 include '../php/DbConfig.php';
 $link = mysqli_connect($server, $user, $pass, $basededatos);
-$sql="SELECT Imagen FROM Usuario WHERE Usuario.Correo = ".$_SESSION['usuario'].";";
-$archivo = $_SESSION['img'];
+$sql="SELECT Imagen FROM Usuario WHERE Usuario.Correo =".$_SESSION['usuario'].";";
+$_SESSION['img'] = $sql;
+
 
 echo"
 <div id='page-wrap'>
 <header class='main' id='h1'>
         <span class='right'><a>".$_SESSION['usuario']."</a>
-        <img src='".base64_decode($archivo)."'>
+        <img src='".base64_decode($sql)."'>
         <span class='right'><a href='LogOut.php'>Logout</a></span>
 </header>
 <nav class='main' id='n1' role='navigation'>
