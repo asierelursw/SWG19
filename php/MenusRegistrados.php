787fcc18fb9ce92b793c1f74123170413236626a
asierelursw <asierelursw@gmail.com>
2020-12-23 12:55:01 +0100

    Update MenusRegistrados.php

--- php/MenusRegistrados.php
+++ php/MenusRegistrados.php
@@ -12,15 +12,15 @@ echo"
 <div id='page-wrap'>
 <header class='main' id='h1'>
         <span class='right'><a>".$_SESSION['usuario']."</a>
-        <br/>
+        
         <img src='".$_SESSION['img']."' height='90px' width='90px'>
-        <br/>
+        
         <span class='right'><a href='LogOut.php'>Logout</a></span>
 </header>
 <nav class='main' id='n1' role='navigation'>
   <span><a href='Layout.php'>Inicio</a></span>
   <span><a href='HandlingQuizesAjax.php'> Insertar/Ver Preguntas</a></span>
-  <span><a href='Credits.php'>Creditos</a></span>
+  <span><a href='RestablecerContrasena.php'>Cambiar Contraseña</a><span>
 </nav>";
 
 ?>
